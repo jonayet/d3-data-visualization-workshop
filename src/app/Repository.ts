70f9095d8bfa6c0e5fbbb6708e590a20d5c9040f
Jonayet Hossain <jonayet.hossain@etventure.com>
2018-04-24 18:49:34 +0200

    HTML Node: FE Rock Stars!

---
+++ src/app/Repository.ts
@@ -0,0 +1,38 @@
+export const feRockStars = [
+  {
+    name: 'Jamie',
+    country: 'US'
+  },
+  {
+    name: 'Brett',
+    country: 'US'
+  },
+  {
+    name: 'Dasha',
+    country: 'RU'
+  },
+  {
+    name: 'Julie',
+    country: 'MX'
+  },
+  {
+    name: 'Gouri',
+    country: 'IN'
+  },
+  {
+    name: 'Sefa',
+    country: 'TR'
+  },
+  {
+    name: 'Aurthur',
+    country: 'BR'
+  },
+  {
+    name: 'Martin',
+    country: 'DE'
+  },
+  {
+    name: 'Jona',
+    country: 'BD'
+  },
+];
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { select, zoom, event, geoMercator, geoPath } from 'd3';
+
+import { feRockStars } from './Repository';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  @ViewChild('mapHost') hostElement: ElementRef;
+
+  ngOnInit() {
+    const htmlNode = this.createHtmlNode(this.hostElement.nativeElement);
+    // const svgNode = this.createSvgNode(this.hostElement.nativeElement, '#ccc');
+
+    const nodes = htmlNode.selectAll('div')
+      .data(feRockStars);
+
+    nodes.enter()
+      .append('div')
+      .style('text-align', 'center')
+      .style('padding', '10px')
+      .text(d => d.name)
+      .append('div')
+      .text(d => d.country);
+  }
+
+  private createHtmlNode(hostElement: any) {
+    return select(hostElement);
+  }
+
+  private createSvgNode(hostElement: any, backgroundColor: string) {
+    const host = select(hostElement);
+    const svg = host.append('svg')
+      .attr('width', '100%')
+      .attr('height', '100%');
+
+    svg.append('rect')
+      .attr('width', '100%')
+      .attr('height', '100%')
+      .style('fill', backgroundColor);
+
+    const rootNode = svg.append('g')
+      .style('fill', backgroundColor);
+
+    const zoomHandler = zoom().on('zoom', () => {
+      const transform = event.transform;
+      rootNode.attr('transform', `translate(${transform.x}, ${transform.y}) scale(${transform.k})`);
+    });
+    host.call(zoomHandler);
+    return rootNode;
+  }
 }
